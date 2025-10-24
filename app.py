9dbed3bc4a49cf898b456d358a9bbe60b40383e8
Dev Portal <dev-portal@local>
2025-10-24 10:12:44 +0700

    fix make res

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, make_response
 import os
 import subprocess
 import json
